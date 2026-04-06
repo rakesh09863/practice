96f0d2c65f415f42e4a1f7255f504ed9638358d5
Rakesh <rakeshsunny0987@gmail.com>
2026-04-06 22:51:12 +0530

    Merge remote-tracking branch 'origin/main'

--- mapex3.py
+++ mapex3.py
@@ -2,7 +2,7 @@ print('Enter the List of values separated by space:')
 old_sal=[float(val) for val in input().split() if float(val)>0]
 print(old_sal)
 new_sal=list(map(lambda sal:sal+sal*50/100,old_sal))
-print(new_sal)
+print(new_sal)#10 20 30 -1 -4 -3 20 4
 print('Old sal \t new sal')
 for k,v in zip(old_sal,new_sal):
     print('{}---->{}'.format(k,v))
